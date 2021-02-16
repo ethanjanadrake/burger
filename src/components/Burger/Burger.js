9786615ea126d5ab80710f052fd6fc9a0594f8e6
Ethan Drake <ethanjanadrake@gmail.com>
2021-02-15 20:58:53 -0800

    129

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
 const burger = (props) => {
-	return <div className='' />;
+	// Object.keys() returns an array of the object's keys
+	const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
+		return [
+			...Array(props.ingredients[igKey])
+		].map((_, i) => {
+			return <BurgerIngredient key={igKey + i} type={igKey} />;
+		});
+	});
+	return (
+		<div className={classes.Burger}>
+			<BurgerIngredient type='bread-top' />
+			{transformedIngredients}
+			<BurgerIngredient type='bread-bottom' />
+		</div>
+	);
 };
 
 export default burger;
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,4 +1,3 @@
-import classes from '*.module.css';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
